76a83aa5d6125998e0c822744a95d7775a2a80bd
Ferdy Gondo <ferdy.gondo@hypermediasystems.com>
2022-05-13 21:47:37 -0700

    fix useVideos.js

--- src/hooks/useVideos.js
+++ src/hooks/useVideos.js
@@ -1,10 +1,9 @@
-import React, {useState, useEffect} from "react";
+import {useState, useEffect} from "react";
 import youtube from "../apis/youtube";
 
 const useVideos = (defaultSearchTerm) => {
     const [videos, setVideos] = useState([]);
-    const [selectedVideo, setSelectedVideo] = useState(null);
-
+    // const [selectedVideo, setSelectedVideo] = useState(null);
 
     useEffect( () => {
         onSearch(defaultSearchTerm);
